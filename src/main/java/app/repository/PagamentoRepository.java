b8193b3427e5a2c4fa6efceed481c2d4e9fe37b3
Cássio dos Santos <cassiovsd@hotmail.com>
2024-04-15 21:16:57 -0300

    commit inicial na branch do jean

--- src/main/java/app/repository/PagamentoRepository.java
+++ src/main/java/app/repository/PagamentoRepository.java
@@ -1,5 +1,14 @@
 package app.repository;
 
-public class PagamentoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import app.entity.Pagamento;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long>{
+	
+	public List<Pagamento> findByName(String nome);
 
 }
+
--- src/main/java/app/service/FuncionarioService.java
+++ src/main/java/app/service/FuncionarioService.java
@@ -1,5 +1,31 @@
 package app.service;
 
-public class FuncionarioService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Funcionario;
+import app.repository.FuncionarioRepository;
+
+@Service
+public class FuncionarioService {
+	
+	
+	@Autowired
+	private FuncionarioRepository repository;
+	
+	public String save(Funcionario obj) {
+		
+		if(obj == null)
+			throw new RuntimeException();
+		
+		this.repository.save(obj);
+		return ("Salvo com sucesso!");
+	}
+	
+	public List<Funcionario> listAll(){
+		
+		return this.repository.findAll();
+	}
 }
